1d0bc71872b3c6a6dd384ba1a5a8b746cd2c6918
Elsinor <kr3at00r@gmail.com>
2019-11-11 10:14:27 +0100

    Update callback_info.cc

--- src/callback_info.cc
+++ src/callback_info.cc
@@ -209,7 +209,8 @@ void CallbackInfo::Initialize(Local<Object> target) {
   Nan::HandleScope scope;
 
 	Nan::Set(target, Nan::New<String>("Callback").ToLocalChecked(),
-		Nan::New<FunctionTemplate>(Callback)->GetFunction());
+		Nan::GetFunction(Nan::New<FunctionTemplate>(Callback)).ToLocalChecked()
+	);
 
   // initialize our threaded invokation stuff
 #ifdef WIN32
